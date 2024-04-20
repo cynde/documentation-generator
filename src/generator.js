92116a7e27145349676f1f4b16fb7c2116da5c4e
cynde <cynthiacyndewi@gmail.com>
2024-04-20 15:48:18 +0800

    create mdx parser file

--- src/generator.js
+++ src/generator.js
@@ -1,11 +1,11 @@
-const readFile = (path) => {
-    const fs = require('fs');
+const fs = require('fs');
+const parseMdxContentToHtml = require('./parser/MDXParser');
 
+const readFile = (path) => {
     return fs.readFileSync(path, 'utf8');
 };
 
 const writeFile = (path, data) => {
-    const fs = require('fs');
 
     fs.copyFile('templates/styles/style.css', 'out/styles/style.css', (error) => {
         if (error) {
@@ -22,43 +22,6 @@ const writeFile = (path, data) => {
         }
     });
 };
-
-const parseMdxContentToHtml = (content) => {
-    // Code blocks
-    content = content.replace(/```([^ \t\n]+)?\n((?:.|\n)*?)\n```/gm, function(match, language, code) {
-        const className = language ? `language-${language.toLowerCase()}` : "";
-        return `<pre class="${className}"><code>${code.trim()}</code></pre>`;
-    });
-
-    // Inline code
-    content = content.replace(/`([^`]+)`/g, '<code>$1</code>');
-
-    // Hash (#) for headings
-    content = content.replace(/^(#{1,6}) (.*)$/gm, function(match, hash, content) {
-        return `<h${hash.length}>${content}</h${hash.length}>`;
-    });
-
-    // Asterisks (*) for bold and italics
-    content = content.replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>");
-    content = content.replace(/\*(.*?)\*/g, "<em>$1</em>");
-
-    // Underline (_) for italics
-    content = content.replace(/\_(.*?)\_/g, "<em>$1</em>");
-
-    // Paragraph
-    content = content.replace(/\n\n/g, "</p><p>");
-
-    // Links
-    content = content.replace(/(\[(.*?)\]\((.*?)\))|(<(.+?@.+?)>)/g, function(match, innerText, linkText, href, email) {
-        if (linkText && href) {
-            return `<a href="${href}">${linkText}</a>`;
-        } else if (email) {
-            return `<a href="mailto:${email}">${email}</a>`;
-        }
-    });
-
-    return content;
-}
 
 const generateHTML = (template, content) => {
     return template.replace(/{{ mdx-content }}/g, content);
